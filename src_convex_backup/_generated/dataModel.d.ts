4760e2f944f8914f56efdfe100ab2c16431a5434
David Osei Agyemang <devagyemang@gmail.com>
2026-06-11 12:49:28 +0000

    chore: permanently remove convex backup folder

--- src_convex_backup/_generated/dataModel.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import type { GenericId } from "convex/values";
-
-export type Id<TableName extends string> = GenericId<TableName>;
-export type Doc<TableName extends string> = Record<string, any> & {
-  _id: Id<TableName>;
-  _creationTime: number;
-};
